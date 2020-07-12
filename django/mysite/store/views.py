9821ead4ca6ea2f6b1fcfcd2e2c49ff8624a6a8f
minnb <minhnb.it@gmail.com>
2020-07-12 14:23:23 +0700

    rm

--- django/mysite/store/views.py
+++ django/mysite/store/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
-def  store(request):
-    context = {}
+def store(request):
+    products = Product.objects.all()
+    context = {'products':products}
     return render(request, "store/store.html", context)
+
 
 def main(request):
     context = {}
     return render(request, "store/main.html", context)
 
+
 def cart(request):
     context = {}
     return render(request, "store/cart.html", context)
+
 
 def checkout(request):
     context = {}
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://github.com/divanov11/ecom_steps
