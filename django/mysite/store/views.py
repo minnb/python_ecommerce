4c54d46bd3495f5b7770f05d3d0c18196a2086b1
minnb <minhnb.it@gmail.com>
2020-08-25 21:27:05 +0700

    A

--- django/mysite/store/views.py
+++ django/mysite/store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-import  json
+import json
 from .models import *
 import datetime
 
@@ -82,5 +82,28 @@ def updateItem(request):
 
 
 def processOrder(request):
+    print("data", request.body)
     transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == float(order.get_cart_total):
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address  = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+    else:
+        print('USer is not logged in ...')
     return JsonResponse('Payment complete!', safe=False)
